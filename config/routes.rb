67f53404105b8e5a4070469fa04f91fe667f9f72
Chanya Kukulprasong <chanjeedtu75@gmail.com>
2021-06-18 16:47:18 +0900

    station4

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :movies
   get 'admin/movies', to: 'movies#index'
+  get 'admin/movies/:id', to: 'movies#show'
   get 'admin/movies/new', to: 'movies#new'
   post 'admin/movies', to: 'movies#create'
 end
